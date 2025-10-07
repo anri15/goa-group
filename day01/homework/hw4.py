d8fcb01cea27434a4d05f8acba62424ae2fb131c
anri15 <anri.malania15@gmail.com>
2025-10-07 12:05:23 -0700

    first commit

---
+++ day01/homework/hw4.py
@@ -0,0 +1,12 @@
+# 4)დაპირნტტეთ თქვენი სახელი 10 ჯერ
+print('anri')
+print('anri')
+print('anri')
+print('anri')
+print('anri')
+print('anri')
+print('anri')
+print('anri')
+print('anri')
+print('anri')
+
