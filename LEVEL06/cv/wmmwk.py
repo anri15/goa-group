a5e2eacc688d332679b74d02e362456273a53209
anri15 <anri.malania15@gmail.com>
2025-11-10 03:23:32 -0800

    second commit

---
+++ LEVEL06/cv/wmmwk.py
@@ -0,0 +1,7 @@
+print(10<9)
+print(67>69)
+print(61>67)
+
+print(54>23)
+print(98<172)
+print(56>41)
---
+++ LEVEL06/hv/4.py
@@ -0,0 +1,5 @@
+#4)შექმენით 2 ცვლადი სადაც შეეკითხებოდით რენდომ რიცხვებს,შემდეგ კი შეადარებთ ერთმანეთს
+
+num3 = input("enter your age: ")
+num = input("enter your ager: ")
+print(num3<num)
