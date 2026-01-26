8e100e071a88b5cab3bd9600c6732e72a4bdc974
anri15 <anri.malania15@gmail.com>
2026-01-26 10:33:28 -0800

    second commit

---
+++ level15/hw/3.py
@@ -0,0 +1,6 @@
+#3️⃣
+#მოცემულია სია
+#animals = ['cat', 'dog', 'fox', 'wolf', 'bear', 'lion']
+#ამოიღე ელემენტები 'dog'-დან 'bear'-მდე (ჩათვლით).
+animals = ['cat', 'dog', 'fox', 'wolf', 'bear', 'lion']
+print(animals[1:5])
---
+++ level15/hw/5.py
@@ -0,0 +1,6 @@
+#5️⃣
+#მოცემულია სია
+#days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+#ამოიღე ელემენტები 'tue'-დან 'fri'-მდე (ჩათვლით).
+days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+print(days[1:5])
